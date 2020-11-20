5f14ffd0ea341b0adf60c8b022792a1eb8d25365
Isaac <finken.isaac@gmail.com>
2020-11-20 14:33:53 -0600

    Removed extra data and favicon - hopefully flower shows up.

--- starter_app/__init__.py
+++ starter_app/__init__.py
@@ -58,6 +58,7 @@ def load_user(user_id):
 @app.route('/<path:path>')
 def react_root(path):
     print("path", path)
+    print('--------------------------------------',path)
     if path == 'favicon.ico':
         return app.send_static_file('favicon.ico')
     return app.send_static_file('index.html')
